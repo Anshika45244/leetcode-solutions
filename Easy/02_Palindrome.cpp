8831eba6054d14ed26805b41920835c4edcd59c0
Anshika Verma <anshikaverma6334@gmail.com>
2025-08-04 12:31:38 -0400

    Create 02_Palindrome.

---
+++ Easy/02_Palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers and ending with 0 (except 0 itself) can't be palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+
+        // For even length: x == reversed
+        // For odd length: x == reversed/10
+        return (x == reversed || x == reversed / 10);
+    }
+};
